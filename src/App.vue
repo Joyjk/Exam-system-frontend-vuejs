edfb8c3ea1b504800f9de747a9adf5fd481baa79
Joy <joyjk99@gmail.com>
2022-05-29 10:46:19 +0600

    added updated

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <!-- <router-link to="/">Home</router-link> | -->
       <!-- <router-link to="/about">About</router-link> | -->
       <!-- <router-link to="/page2">Page 2</router-link> | -->
-      <router-link to="/addnewuser">New User</router-link> |
-      <router-link to="/addcandidate">New Candidate</router-link> |
-      <router-link to="/addcandidate2">New short Candidate</router-link> |
+      
+      
+      <router-link to="/addcandidate2">Candidate 1st page</router-link> |
       <router-link to="/secondpage">Second Page</router-link> |
+      <router-link to="/tebtest">Questions</router-link>  |
+      
       <!-- <router-link to="/questiontype1">Question type 1</router-link> | -->
       <!-- <router-link to="/questiontype2">Question type 2</router-link> | 
       <router-link to="/questiontype3">Question type 3</router-link> | -->
       <!-- Question type 4 -->
       <!-- <router-link to="/questiontype4">Questions</router-link> | -->
       <router-link to="/login">Login</router-link> |
+      <router-link to="/addnewuser">New User</router-link> |
+      <!-- <router-link to="/addcandidate">New Candidate</router-link> | -->
       <router-link to="/hrsetupcandidate">Hr setup candidate</router-link> |
       <router-link to="/setupquestion">Setup Question</router-link> |
       <router-link to="/questionset">Question Set</router-link>   |
@@ -22,7 +26,7 @@
       <router-link to="/viewexam">View exam paper</router-link> |
       <router-link to="/resultsheet">Result Sheet</router-link>  |
       <!-- <router-link to="/questionnew">Question new try</router-link> -->
-      <router-link to="/tebtest">Questions</router-link>  |
+      
       <router-link to="/addsetname">Add Set Name</router-link>  |
       <router-link to="/addcandidatetype">Add Candidate type</router-link>  |
       <router-link to="/addcandidatesession">Add Candidate Session</router-link>  |
